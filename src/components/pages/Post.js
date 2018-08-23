22530a9a3806d9546b9bf5228398029edd9ca5e8
Christian Geroy <cr50.christian@enfrausaph.com>
2018-08-23 13:22:01 -0400

    Adding the Blog API

--- src/components/pages/Post.js
+++ src/components/pages/Post.js
@@ -4,24 +4,57 @@ class Post extends Component {
 
   state = {
     currentUserName: '',
-    currentUserEmail: ''
+    currentUserEmail: '',
+    posts: []
+  }
+
+  getPosts = () => {
+    fetch('https://xchanrey-blog-api.herokuapp.com/posts')
+      .then(res => res.json())
+      .then(data => data.map(post => (
+        {
+          id: `${post.id}`,
+          title: `${post.title}`,
+          body: `${post.body}`
+        }
+
+      )))
+      .then(posts => this.setState({
+        posts
+      }))
+      .catch(err => console(err))
   }
 
   componentDidMount(){
     const idToken = JSON.parse(localStorage.getItem('okta-token-storage'));
     this.setState({
       currentUserEmail: idToken.idToken.claims.email,
-      currentUserName: idToken.idToken.claims.name
+      currentUserName: idToken.idToken.claims.name,
     });
+
+    this.getPosts();
   }
   render() {
-    const { currentUserEmail, currentUserName } = this.state;
+    const { currentUserName, posts } = this.state;
     return (
       <div>
         <h1>Welcome { currentUserName }</h1>
         <p>You have reached the point of no return!</p>
+        <div>
+          {posts.map(({id, title, body}) => {
+            return 
+                  (
+                    <div className="card" key={id}>
+                      <div className="card-body">
+                        <h2>{title}</h2>
+                        <p>{body}</p>
+                      </div>
+                    </div>
+                  );
+          })}
+        </div>
       </div>
-    )
+    );
   }
 }
 
