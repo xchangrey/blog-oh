06819cfa7eba6f651db55448e3a871d4c68d4cb8
Christian Geroy <cr50.christian@enfrausaph.com>
2018-08-23 11:39:21 -0400

    Finishing the app

--- src/components/pages/Post.js
+++ src/components/pages/Post.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default () => {
-  return (
-    <div>
-      <h1>Posts</h1>
-    </div>
-  )
+class Post extends Component {
+
+  state = {
+    currentUserName: '',
+    currentUserEmail: ''
+  }
+
+  componentDidMount(){
+    const idToken = JSON.parse(localStorage.getItem('okta-token-storage'));
+    this.setState({
+      currentUserEmail: idToken.idToken.claims.email,
+      currentUserName: idToken.idToken.claims.name
+    });
+  }
+  render() {
+    const { currentUserEmail, currentUserName } = this.state;
+    return (
+      <div>
+        <h1>Welcome { currentUserName }</h1>
+        <p>You have reached the point of no return!</p>
+      </div>
+    )
+  }
 }
+
+export default Post;
